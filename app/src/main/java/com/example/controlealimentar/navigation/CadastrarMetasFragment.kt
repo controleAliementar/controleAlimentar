f4031047a9aa48d1085789222e0c26424ad72aed
Your Name <luiz.henrique30@hotmail.com>
2020-03-24 20:11:43 -0300

    feature: adicionando validação de campos na tela de metas diarias

---
+++ app/src/main/java/com/example/controlealimentar/navigation/CadastrarMetasFragment.kt
@@ -0,0 +1,205 @@
+package com.example.controlealimentar.navigation
+
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.controlealimentar.R
+import com.example.controlealimentar.databinding.FragmentCadastrarMetasBinding
+import com.example.controlealimentar.exception.SalvarMetaDiariasException
+import com.example.controlealimentar.model.MetaDiarias
+import com.example.controlealimentar.model.enuns.SharedIds
+import com.example.controlealimentar.service.MetaDiariasService
+import com.example.controlealimentar.util.Loading
+import com.example.controlealimentar.util.SharedPreference
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class CadastrarMetasFragment : Fragment() {
+
+    private val metaDiariasService : MetaDiariasService =
+        MetaDiariasService()
+
+    lateinit var binding : FragmentCadastrarMetasBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil
+            .inflate(inflater,
+                R.layout.fragment_cadastrar_metas, container, false)
+
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        binding.salvarButton.setOnClickListener {
+
+            val loading = Loading(context)
+
+            try {
+
+                val sharedPreference = SharedPreference(context)
+                val processoId : String? = sharedPreference.getValueString(SharedIds.ID_USUARIO.name)
+
+                if (processoId.isNullOrBlank()){
+                    throw SalvarMetaDiariasException("ProcessoId não encontrado no SharedPreference")
+                }
+
+                val gordura = java.lang.Double.parseDouble(binding.gorduraText.text.toString())
+                val carboidrato = java.lang.Double.parseDouble(binding.carboidratoText.text.toString())
+                val proteina = java.lang.Double.parseDouble(binding.proteinaText.text.toString())
+                val calorias = java.lang.Double.parseDouble(binding.caloriaText.text.toString())
+
+                val metaDiarias = MetaDiarias()
+                metaDiarias.gorduras = gordura
+                metaDiarias.carboidratos = carboidrato
+                metaDiarias.proteinas = proteina
+                metaDiarias.calorias = calorias
+                metaDiarias.processoId = processoId
+
+                metaDiariasService.salvarMetaDiarias(processoId, metaDiarias)
+
+                loading.remover()
+
+                val action =
+                    CadastrarMetasFragmentDirections.actionSalvarMetasFragmentToHomeFragment()
+                view?.findNavController()?.navigate(action)
+            } catch (e : Exception){
+                loading.remover()
+
+                val action =
+                    CadastrarMetasFragmentDirections.actionSalvarMetasFragmentToErroGenericoFragment()
+                view?.findNavController()?.navigate(action)
+            }
+        }
+
+        binding.salvarButton.isEnabled = false
+
+        var editText1IsNull = true
+        var editText2IsNull = true
+        var editText3IsNull = true
+        var editText4IsNull = true
+
+        binding.caloriaText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(text: Editable?) {}
+
+            override fun beforeTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                editText1IsNull = text.isNullOrBlank()
+                habilitarBotao(editText1IsNull, editText2IsNull, editText3IsNull, editText4IsNull)
+                validarDadosEntrada(binding.caloriaText, text.toString())
+            }
+        })
+
+        binding.carboidratoText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(text: Editable?) {}
+
+            override fun beforeTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                editText2IsNull = text.isNullOrBlank()
+                habilitarBotao(editText1IsNull, editText2IsNull, editText3IsNull, editText4IsNull)
+                validarDadosEntrada(binding.carboidratoText, text.toString())
+            }
+        })
+
+        binding.proteinaText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(text: Editable?) {}
+
+            override fun beforeTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                editText3IsNull = text.isNullOrBlank()
+                habilitarBotao(editText1IsNull, editText2IsNull, editText3IsNull, editText4IsNull)
+                validarDadosEntrada(binding.proteinaText, text.toString())
+            }
+        })
+
+        binding.gorduraText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(text: Editable?) {}
+
+            override fun beforeTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                editText4IsNull = text.isNullOrBlank()
+                habilitarBotao(editText1IsNull, editText2IsNull, editText3IsNull, editText4IsNull)
+                validarDadosEntrada(binding.gorduraText, text.toString())
+            }
+        })
+
+    }
+
+    private fun habilitarBotao(editText1IsNull: Boolean, editText2IsNull: Boolean,
+                               editText3IsNull: Boolean, editText4IsNull: Boolean) {
+        binding.salvarButton.isEnabled = !editText1IsNull && !editText2IsNull
+                && !editText3IsNull && !editText4IsNull
+    }
+
+    fun validarDadosEntrada(ediTxt: EditText, text : String){
+
+        // Colocar ponto após digitação do quarto numero
+        if (!text.contains('.') && text.length > 4){
+            val ultimoDigito = text.substring(4)
+            val textFormatado = text.substring(0, 4) + "." + ultimoDigito
+            ediTxt.setText(textFormatado)
+            ediTxt.setSelection(textFormatado.length)
+        }
+
+        // Não deixar começar com ponto
+        if (text.startsWith('.')){
+            val replace = text.replace(".", "")
+            ediTxt.setText(replace)
+            ediTxt.setSelection(replace.length)
+        }
+
+        // Não deixar ter mais de cinco casas o numero
+        val textSemPonto = text.replace(".", "")
+        if (textSemPonto.length > 5){
+            val substring = text.substring(0, text.lastIndex)
+            ediTxt.setText(substring)
+            ediTxt.setSelection(substring.length)
+        }
+
+        // Não deixar terminar com ponto
+        if (text.length == 5 && text.endsWith(".")){
+            val replace = text.replace(".", "")
+            ediTxt.setText(replace)
+            ediTxt.setSelection(replace.length)
+        }
+
+
+        // Não deixar ter mais de um ponto
+        val quantidadePontos = text.count { string -> string.equals('.') }
+        if (quantidadePontos > 1){
+            val replace = text.replace(".", "")
+            ediTxt.setText(replace)
+            ediTxt.setSelection(replace.length)
+        }
+
+
+        // Não deixar ter mais de duas casas depois do ponto
+        val indexPonto = text.indexOf('.')
+        val ultimoIndex = text.lastIndex
+
+        if (indexPonto != -1 && (ultimoIndex - indexPonto >= 2)){
+            val substring = text.substring(0, text.lastIndex)
+            ediTxt.setText(substring)
+            ediTxt.setSelection(substring.length)
+        }
+
+    }
+
+}
